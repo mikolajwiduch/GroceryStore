4ac927cae1ce521c3a6bb1c573c23dd47270b839
Mikołaj Widuch <mikolajwiduch@gmail.com>
2024-08-23 18:53:28 +0200

    Add VievModels. Views (not working yet), source data

---
+++ GroceryStore/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using GroceryStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStore.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public bool Login()
+        {
+            var users = User.LoadUsers("Data/users.csv");
+            return User.Authenticate(Username, Password, users);
+        }
+    }
+
+}
---
+++ GroceryStore/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using GroceryStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStore.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Product> Products { get; set; }
+        public Order CurrentOrder { get; set; }
+
+        public MainViewModel()
+        {
+            Products = new ObservableCollection<Product>(Product.LoadProducts("Data/products.csv"));
+            CurrentOrder = new Order();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+}
